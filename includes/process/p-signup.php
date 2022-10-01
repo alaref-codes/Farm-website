d5d412962a9b78acc8fa072d761ed0e75018d3d2
alaref-codes <alarefabushaala@gmail.com>
2022-10-01 22:41:59 +0200

    Added data validation for the sign up page

--- includes/process/p-signup.php
+++ includes/process/p-signup.php
@@ -8,35 +8,35 @@
     {        
         $username = $_POST['username'];    
         $farmname = $_POST['farmname'];            
-        // if ($h->isEmpty(array($username, $_POST['password'], $_POST['confirm_password'])))
-        // {
-        //     $msg = 'All fields are required';     
-        // }
-        // else if (!$h->isValidLength($username, 6, 100)){
-        //     $msg = 'Username must be between 6 and 100 characters';
-        // }
-        // else if (!$h->isValidLength($_POST['password'])){
-        //     $msg = 'Password must be between 8 and 20 characters';
-        // }
-        // else if (!$h->isSecure($_POST['password'])){
-        //     $msg = 'Password must contain at least one lowercase character, one uppercase character and one digit';
-        // }
-        // else if (!$h->passwordsMatch($_POST['password'], $_POST['confirm_password'])){
-        //     $msg = 'Passwords do not match';
-        // }        
-        // else
-        // {
-        $user = new FarmUser($username, $farmname);
+        if ($h->isEmpty(array($username, $_POST['password'], $_POST['farmname'], $_POST['confirm_password'])))
+        {
+            $msg = 'All fields are required';     
+        }
+        else if (!$h->isValidLength($username, 6, 100)){
+            $msg = 'Username must be between 6 and 100 characters';
+        }
+        else if (!$h->isValidLength($_POST['password'])){
+            $msg = 'Password must be between 8 and 20 characters';
+        }
+        else if (!$h->isSecure($_POST['password'])){
+            $msg = 'Password must contain at least one lowercase character, one uppercase character and one digit';
+        }
+        else if (!$h->passwordsMatch($_POST['password'], $_POST['confirm_password'])){
+            $msg = 'Passwords do not match';
+        }        
+        else
+        {
+            $user = new FarmUser($username, $farmname);
 
 
             // if ($member->isDuplicateID())
             // {
             //     $msg = "Username already in use";
             // }
-         
-        $user->insertIntoFarmDB($_POST['password']);
+        
+            $user->insertIntoFarmDB($_POST['password']);
 
-        header("Location: index.php?new=1");                
-        // }
+            header("Location: index.php?new=1");                
+            }
             
     }
